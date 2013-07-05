43848807ea14033bd0f3be8452523f740341b87a
rousself <rousself@gmail.com>
2013-07-05 10:01:18 +0200

    35

--- js/config.js
+++ js/config.js
@@ -4,5 +4,7 @@ socket_io: {url: 'http://test-fred.emsc-csem.org:8082/test'},
 settings: {timers:2, min_mag:1,notPos:'RC',screenAlert:true,shakeAlert:true,shakeAlertMag:1,audioAlert:true,audioAlertMag:2},
 audio: {url: 'http://test-fred.emsc-csem.org:8080/Tools/Audio/',test:{mag:4.5,region:'CENTRAL ITALY',ago:4}},
 video: {url: 'http://test-fred.emsc-csem.org:8080/Earthquake/Contribute/Pictures/upload_multi.php',params:{evid:0,lat:0,lng:0}},
+
+android: {senderID:'869856509191'},
 debug:true
 };
--- js/main.js
+++ js/main.js
@@ -265,9 +265,99 @@ function AudioAlert() {
  function onDeviceReady2() { 
 	console.log('window begin'); 
 	app.initapp(); 
-
+	Push();
  }
 
+ 
+ function Push() {
+	try {
+		pushNotification = window.plugins.pushNotification;
+		if (device.platform == 'android' || device.platform == 'Android') {
+			$("#app-status-ul").append('<li>registering android</li>');
+			pushNotification.register(successPushH , errorPushH , {"senderID":EmscConfig.android.senderID,"ecb":"onNotificationGCM"});		// required!
+		} else {
+			$("#app-status-ul").append('<li>registering iOS</li>');
+			pushNotification.register(tokenHandler, errorPushH, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
+		}	
+    }catch(error) { console.log('error push register '+error.message);}            	
+ }
+function successPushH (result) {
+    $("#app-status-ul").append('<li>success:'+ result +'</li>');
+}         
+function errorPushH (error) {
+	$("#app-status-ul").append('<li>error:'+ error +'</li>');
+}
+function tokenHandler (result) {
+	$("#app-status-ul").append('<li>token: '+ result +'</li>');
+	// Your iOS push server needs to know the token before it can push to this device
+	// here is where you might want to send it the token for later use.
+}
+  // handle APNS notifications for iOS
+function onNotificationAPN(e) {
+	if (e.alert) {
+		 $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+		 navigator.notification.alert(e.alert);
+	}
+		
+	if (e.sound) {
+		var snd = new Media(e.sound);
+		snd.play();
+	}
+	
+	if (e.badge) {
+		pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+	}
+}
+
+// handle GCM notifications for Android
+function onNotificationGCM(e) {
+	$("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+	
+	switch( e.event ) {
+		case 'registered':
+		if ( e.regid.length > 0 ) {
+			$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+			// Your GCM push server needs to know the regID before it can push to this device
+			// here is where you might want to send it the regID for later use.
+			console.log("regID = " + e.regID);
+		}
+		break;
+		
+		case 'message':
+			// if this flag is set, this notification happened while we were in the foreground.
+			// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+			if (e.foreground) {
+				$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+				// if the notification contains a soundname, play it.
+				var my_media = new Media("/android_asset/www/"+e.soundname);
+				my_media.play();
+			}
+			else {	// otherwise we were launched because the user touched a notification in the notification tray.
+				if (e.coldstart)
+					$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+				else
+				$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+			}
+
+			$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+			$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+		break;
+		
+		case 'error':
+			$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+		break;
+		
+		default:
+			$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+		break;
+	}
+}
+
+ 
+ 
+ 
+ 
  
  $(function() {
     document.addEventListener("deviceready", function() { console.log('doc2 begin');  }, true);
